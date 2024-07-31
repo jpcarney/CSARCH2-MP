87882c77d6172219bfa83d0a39f441428eac7a7e
jpcarney <125584245+jpcarney@users.noreply.github.com>
2024-07-31 04:52:51 +0000

    test for servlet

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,16 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
 
+@WebServlet("/simulateServlet")
+public class SimulateServlet extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+ 
+        // code to process the form...
+ 
+    }
+ 
+}
